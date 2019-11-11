5d9705accf48cc4bc9845e60be2982f5645137b8
8c6t <nkw8620@gmail.com>
2019-11-11 16:38:00 +0900

    1.6 스프링 시큐리티 커스터마이징: JPA 연동

--- ch01/demo-spring-security-form/src/main/java/com/hachicore/demospringsecurityform/config/SecurityConfig.java
+++ ch01/demo-spring-security-form/src/main/java/com/hachicore/demospringsecurityform/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.hachicore.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,20 +12,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
         http.formLogin();
         http.httpBasic();
     }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("hachicore").password("{noop}123").roles("USER").and()
-                .withUser("admin").password("{noop}!@#").roles("ADMIN");
-    }
-
 
 }
