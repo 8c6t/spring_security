dc0d77f01a91513eae163144c5ee4adaeb0e78c8
8c6t <nkw8620@gmail.com>
2019-11-12 12:43:36 +0900

    2.5 스프링 시큐리티 필터와 FilterChainProxy

--- ch02/demo-spring-security-form/src/main/java/com/hachicore/demospringsecurityform/config/SecurityConfig.java
+++ ch02/demo-spring-security-form/src/main/java/com/hachicore/demospringsecurityform/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.hachicore.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 50)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -15,6 +18,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
+
+//        http
+//                .antMatcher("/account/**")
+//                .authorizeRequests()
+//                .anyRequest().permitAll();
 
         http.formLogin();
         http.httpBasic();
